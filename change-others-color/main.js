bcac87b0a1f64607e362f8e45cb23c99e48eeffa
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-28 18:04:33 +0100

    Point 3. Change others color.

---
+++ change-others-color/main.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("button_changing_span_color");
+let span = document.getElementById("span_changing_color");
+
+
+button.addEventListener("click", function () {
+    span.style.backgroundColor = "red";
+
+});
